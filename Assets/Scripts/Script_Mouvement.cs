0920bcd69385e4fdccbf57b665ab93dc08816ac9
Nidigashan Kanagendran <123741227+NidiMeliodas@users.noreply.github.com>
2024-06-26 01:47:05 +0200

    new mouvmenet script + new way  ground check + implementing basic animation (idle - run - jump) + level menu update

--- Assets/Scripts/Script_Mouvement.cs
+++ Assets/Scripts/Script_Mouvement.cs
@@ -1,28 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks.Sources;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Script_Mouvement : MonoBehaviour
 {
+    // Composants
     private Rigidbody2D rb;
+    private Animator anim;
 
+    // Mouvement horizontal
     private float move;
-
     public float Speed;
+    private bool isFacingRight;
+
+    // Saut
     public float jump;
+
+    // Détection du sol
     public Vector2 boxSize;
     public float castDistance;
     public float castHorizontalOffset;
     public LayerMask LevelLayer;
-    bool Grounded;
-    // Start is called before the first frame update
+
+    // Initialisation
     void Start()
     {
+        isFacingRight = true;
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // Mise à jour
     void Update()
     {
         move = Input.GetAxisRaw("Horizontal");
@@ -33,11 +44,42 @@ public class Script_Mouvement : MonoBehaviour
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
         }
+
+        if (move != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        anim.SetBool("isJumping", !isGrounded());
+
+        if (!isFacingRight && move > 0)
+        {
+            Flip();
+        }
+        else if (isFacingRight && move < 0)
+        {
+            Flip();
+        }
     }
 
+    // Inversion de direction
+    public void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    // Vérification au sol
     public bool isGrounded()
     {
-        if (Physics2D.BoxCast(transform.position, boxSize, 1, -transform.up, castDistance, LevelLayer))
+        Vector2 castPosition = new Vector2(transform.position.x + castHorizontalOffset, transform.position.y);
+        if (Physics2D.BoxCast(castPosition, boxSize, 0, -Vector2.up, castDistance, LevelLayer))
         {
             return true;
         }
@@ -47,9 +89,24 @@ public class Script_Mouvement : MonoBehaviour
         }
     }
 
+    // Dessin des Gizmos
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+        Vector2 gizmoPosition = new Vector2(transform.position.x + castHorizontalOffset, transform.position.y);
+        Gizmos.DrawWireCube(gizmoPosition - Vector2.up * castDistance, boxSize);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Killzone"))
+        {
+            RestartLevel();
+        }
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     // private void OnCollisionEnter2D(Collision2D other)
@@ -71,5 +128,4 @@ public class Script_Mouvement : MonoBehaviour
     //         Grounded = false;
     //     }
     // }
-
 }
